42ef63dcb2f0bca2989141809b5df6935e2fa6b0
BEUVE <beuve225@unicaen.fr>
2025-03-25 23:30:53 +0100

    :construction: first ideas

--- src/main/java/com/example/sae4_project/Controller/AgarioController.java
+++ src/main/java/com/example/sae4_project/Controller/AgarioController.java
@@ -16,6 +16,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -37,14 +38,13 @@ public class AgarioController extends Controller {
     private double posY;
 
     private Map map = Map.getInstance();
-    private Camera cam = new Camera(new Coordinate(0,0));
+    private Camera cam = new Camera(new Coordinate(0, 0));
 
     @FXML
     private void addCircle(Circle circle) {
 
         this.terrain.getChildren().add(circle);
     }
-
 
 
     @Override
@@ -56,11 +56,12 @@ public class AgarioController extends Controller {
         this.terrain.setLayoutX(0);
         this.terrain.setLayoutY(0);
 
-        this.miniMap.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+
 
         map.getAllPellet(map.getQuadTree(), allPellets);
 
-        for(Pellet elm : allPellets){
+        for (Pellet elm : allPellets) {
             addCircle(elm.getCircle());
         }
 
@@ -69,25 +70,57 @@ public class AgarioController extends Controller {
         addCircle(circle);
 
 
-        cam.getCoordinate().XProperty().bind( Bindings.add(
+        cam.getCoordinate().XProperty().bind(Bindings.add(
                 Bindings.multiply(-1,
-                        Bindings.divide( conteneurGlobal.widthProperty(), 2)) , circle.centerXProperty()));
+                        Bindings.divide(conteneurGlobal.widthProperty(), 2)), circle.centerXProperty()));
         cam.getCoordinate().YProperty().bind(Bindings.add(
                 Bindings.multiply(-1,
-                        Bindings.divide( conteneurGlobal.heightProperty(), 2)), circle.centerYProperty()));
-        terrain.translateXProperty().bind(Bindings.multiply(-1,cam.getCoordinate().XProperty()));
-        terrain.translateYProperty().bind(Bindings.multiply(-1,cam.getCoordinate().YProperty()));
+                        Bindings.divide(conteneurGlobal.heightProperty(), 2)), circle.centerYProperty()));
+        terrain.translateXProperty().bind(Bindings.multiply(-1, cam.getCoordinate().XProperty()));
+        terrain.translateYProperty().bind(Bindings.multiply(-1, cam.getCoordinate().YProperty()));
 
 
         this.terrain.addEventHandler(MouseEvent.MOUSE_MOVED, handler);
 
         this.gameLoop();
+
+        miniMap.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+
+/*
+        double scaleX = miniMap.getPrefWidth() / terrain.getPrefWidth();
+        double scaleY = miniMap.getPrefHeight() / terrain.getPrefHeight();
+
+        Circle miniPlayer = new Circle(player.getCircle().getRadius() * scaleX, Color.RED);
+        miniPlayer.centerXProperty().bind(player.getCircle().centerXProperty().multiply(scaleX));
+        miniPlayer.centerYProperty().bind(player.getCircle().centerYProperty().multiply(scaleY));
+        miniMap.getChildren().add(miniPlayer);
+        for (Pellet pellet : allPellets) {
+            Circle miniPellet = new Circle(pellet.getCircle().getRadius() * scaleX, Color.BLUE);
+            miniPellet.setCenterX(pellet.getCircle().getCenterX() * scaleX);
+            miniPellet.setCenterY(pellet.getCircle().getCenterY() * scaleY);
+            miniMap.getChildren().add(miniPellet);
+        }
+        miniMap.widthProperty().addListener((obs, oldVal, newVal) -> {
+            double newScaleX = miniMap.getPrefWidth() / newVal.doubleValue();
+            System.out.println("Nouveau scaleX : " + newScaleX);
+        });
+        miniMap.heightProperty().addListener((obs, oldVal, newVal) -> {
+            double newScaleY = miniMap.getPrefHeight() / newVal.doubleValue();
+            System.out.println("Nouveau scaleY : " + newScaleY);
+        });
+
+        System.out.println("ScaleX: " + scaleX + ", ScaleY: " + scaleY);
+*/
+
+
+
+
     }
 
     EventHandler handler = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent mouseEvent) {
-            player.moveTowards( mouseEvent.getX(), mouseEvent.getY());
+            player.moveTowards(mouseEvent.getX(), mouseEvent.getY());
 
         }
     };
@@ -113,5 +146,6 @@ public class AgarioController extends Controller {
         };
         timer.start();
     }
+
 
 }
