20f3bb31f70eaf60a7ecc14cb94186d8fa8915f3
Lucas Demaimay <demaima231@unicaen.fr>
2025-03-26 10:51:42 +0100

    :construction: Beginning of online

---
+++ src/main/java/Controller/ConnexionController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ConnexionController {
+    @FXML
+    private Button host;
+
+    @FXML
+    private Button join;
+
+    @FXML
+    private TextField ip;
+}
