a98576a656a4e2115ba641a16a201cc8a7419166
BEUVE <beuve225@unicaen.fr>
2025-03-26 09:08:16 +0100

    :construction: always loading

--- src/main/java/com/example/sae4_project/Controller/AgarioController.java
+++ src/main/java/com/example/sae4_project/Controller/AgarioController.java
@@ -49,73 +49,88 @@ public class AgarioController extends Controller {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-
+        // Lier la taille du terrain à la taille du conteneur global
         this.terrain.prefHeightProperty().bind(conteneurGlobal.heightProperty());
         this.terrain.prefWidthProperty().bind(conteneurGlobal.widthProperty());
         this.terrain.setLayoutX(0);
         this.terrain.setLayoutY(0);
 
-
-
-
+        // Charger les pellets et les afficher sur la carte principale
         map.getAllPellet(map.getQuadTree(), allPellets);
-
         for (Pellet elm : allPellets) {
             addCircle(elm.getCircle());
         }
 
+        // Création du joueur et ajout sur la carte
         this.player = new CreatorPlayer().create();
         Circle circle = this.player.getCircle();
         addCircle(circle);
 
-
+        // Liaison de la caméra avec le joueur
         cam.getCoordinate().XProperty().bind(Bindings.add(
-                Bindings.multiply(-1,
-                        Bindings.divide(conteneurGlobal.widthProperty(), 2)), circle.centerXProperty()));
+                Bindings.multiply(-1, Bindings.divide(conteneurGlobal.widthProperty(), 2)),
+                circle.centerXProperty()));
         cam.getCoordinate().YProperty().bind(Bindings.add(
-                Bindings.multiply(-1,
-                        Bindings.divide(conteneurGlobal.heightProperty(), 2)), circle.centerYProperty()));
+                Bindings.multiply(-1, Bindings.divide(conteneurGlobal.heightProperty(), 2)),
+                circle.centerYProperty()));
+
         terrain.translateXProperty().bind(Bindings.multiply(-1, cam.getCoordinate().XProperty()));
         terrain.translateYProperty().bind(Bindings.multiply(-1, cam.getCoordinate().YProperty()));
 
-
+        // Gestion du mouvement du joueur
         this.terrain.addEventHandler(MouseEvent.MOUSE_MOVED, handler);
 
+        // Lancer la boucle de jeu
         this.gameLoop();
 
+        // Configuration de la minimap
         miniMap.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
 
-/*
-        double scaleX = miniMap.getPrefWidth() / terrain.getPrefWidth();
-        double scaleY = miniMap.getPrefHeight() / terrain.getPrefHeight();
-
-        Circle miniPlayer = new Circle(player.getCircle().getRadius() * scaleX, Color.RED);
-        miniPlayer.centerXProperty().bind(player.getCircle().centerXProperty().multiply(scaleX));
-        miniPlayer.centerYProperty().bind(player.getCircle().centerYProperty().multiply(scaleY));
+        // Création du joueur sur la minimap
+        miniPlayer = new Circle(player.getCircle().getRadius(), Color.RED);
         miniMap.getChildren().add(miniPlayer);
+
+        // Ajouter les pellets sur la minimap
         for (Pellet pellet : allPellets) {
-            Circle miniPellet = new Circle(pellet.getCircle().getRadius() * scaleX, Color.BLUE);
-            miniPellet.setCenterX(pellet.getCircle().getCenterX() * scaleX);
-            miniPellet.setCenterY(pellet.getCircle().getCenterY() * scaleY);
+            Circle miniPellet = new Circle(pellet.getCircle().getRadius(), Color.BLUE);
             miniMap.getChildren().add(miniPellet);
         }
-        miniMap.widthProperty().addListener((obs, oldVal, newVal) -> {
-            double newScaleX = miniMap.getPrefWidth() / newVal.doubleValue();
-            System.out.println("Nouveau scaleX : " + newScaleX);
-        });
-        miniMap.heightProperty().addListener((obs, oldVal, newVal) -> {
-            double newScaleY = miniMap.getPrefHeight() / newVal.doubleValue();
-            System.out.println("Nouveau scaleY : " + newScaleY);
-        });
 
-        System.out.println("ScaleX: " + scaleX + ", ScaleY: " + scaleY);
-*/
+        // Mettre à jour les échelles et les positions des objets sur la minimap
+        updateMiniMapScale();
 
-
-
-
+        // Écouteurs pour mettre à jour les dimensions de la minimap dynamiquement
+        terrain.widthProperty().addListener((obs, oldVal, newVal) -> updateMiniMapScale());
+        terrain.heightProperty().addListener((obs, oldVal, newVal) -> updateMiniMapScale());
+        miniMap.widthProperty().addListener((obs, oldVal, newVal) -> updateMiniMapScale());
+        miniMap.heightProperty().addListener((obs, oldVal, newVal) -> updateMiniMapScale());
     }
+
+    /**
+     * Met à jour l'échelle et la position des objets sur la minimap
+     */
+    private void updateMiniMapScale() {
+        double scaleX = miniMap.getWidth() / terrain.getWidth();
+        double scaleY = miniMap.getHeight() / terrain.getHeight();
+
+        // Mise à jour du joueur sur la minimap
+        miniPlayer.setRadius(player.getCircle().getRadius() * scaleX);
+        miniPlayer.centerXProperty().bind(player.getCircle().centerXProperty().multiply(scaleX));
+        miniPlayer.centerYProperty().bind(player.getCircle().centerYProperty().multiply(scaleY));
+
+        // Mise à jour des pellets sur la minimap
+        int index = 1; // Commence à 1 car le miniPlayer est à l'index 0
+        for (Pellet pellet : allPellets) {
+            if (index < miniMap.getChildren().size()) {
+                Circle miniPellet = (Circle) miniMap.getChildren().get(index);
+                miniPellet.setRadius(pellet.getCircle().getRadius() * scaleX);
+                miniPellet.centerXProperty().bind(pellet.getCircle().centerXProperty().multiply(scaleX));
+                miniPellet.centerYProperty().bind(pellet.getCircle().centerYProperty().multiply(scaleY));
+            }
+            index++;
+        }
+    }
+
 
     EventHandler handler = new EventHandler<MouseEvent>() {
         @Override
